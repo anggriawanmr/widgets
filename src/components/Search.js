d2e084357b6abc7b1a604020bb9a421e5b74cb4b
anggriawanmr <anggriawanmr@gmail.com>
2021-11-25 14:00:11 +0700

    Adding search feature :sparkles:

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,15 +2,37 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("Search");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios;
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
     };
 
     search();
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -20,6 +42,7 @@ const Search = () => {
           <input value={term} onChange={(e) => setTerm(e.target.value)} className="input" />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
