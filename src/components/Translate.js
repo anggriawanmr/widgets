bda2443a55f9f984773c483ee07955bde1d5594d
anggriawanmr <anggriawanmr@gmail.com>
2021-11-26 19:56:10 +0700

    Adding Translate feature :sparkles:

---
+++ src/components/Translate.js
@@ -0,0 +1,42 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+
+const options = [
+  {
+    label: "afrikaans",
+    value: "af",
+  },
+  {
+    label: "arabic",
+    value: "ar",
+  },
+  {
+    label: "hindi",
+    value: "hi",
+  },
+];
+
+const Translate = () => {
+  const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default Translate;
